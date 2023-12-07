51665303fa68b981a25486065e8c0ab582895a0f
AsadUllahBuild <asadbuild97@gmail.com>
2023-12-07 23:37:48 +0500

    blogging app

---
+++ build/script/app.js
@@ -0,0 +1,45 @@
+import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+import { auth, db, storage } from './config.js';
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js';
+
+const form = document.querySelector('#signup-form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const repeatPassword = document.querySelector('#repeat-password');
+const uploadPhoto = document.querySelector('#upload-photo');
+const modalMessage = document.querySelector('#modal-message');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    if (password.value !== repeatPassword.value) {
+        console.log('Passwords do not match');
+        modalMessage.innerHTML = 'Passwords do not match';
+        return;
+    }
+
+    const files = uploadPhoto.files[0];
+    const storageRef = ref(storage, firstName.value);
+
+    try {
+        const snapshot = await uploadBytes(storageRef, files);
+        const url = await getDownloadURL(snapshot.ref);
+        const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        const user = userCredential.user;
+        await addDoc(collection(db, "users"), {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            uid: user.uid,
+            profileUrl: url
+        });
+        window.location.href = 'login.html';
+    } catch (error) {
+        const errorMessage = error.message;
+        console.error(errorMessage);
+        modalMessage.innerHTML = errorMessage;
+    }
+});
--- script/home.js
+++ script/home.js
@@ -6,91 +6,52 @@ const currentTime = new Date();
 const currentHour = currentTime.getHours();
 const time = document.querySelector('#time');
 
-// Times of the day started
-
-let greeting;
-if (currentHour >= 5 && currentHour < 12) {
-    greeting = 'Good Morning';
-} else if (currentHour >= 12 && currentHour < 17) {
-    greeting = 'Good Afternoon';
-} else if (currentHour >= 17 && currentHour < 21) {
-    greeting = 'Good Evening';
-} else {
-    greeting = 'Good Night';
-}
-console.log(greeting);
-
-const text = document.createTextNode(`${greeting} Readers!`);
-time.appendChild(text);
-
-// Times of the day ended
-
-const fetchUserData = async () => {
-    const usersQuerySnapshot = await getDocs(collection(db, "users"));
-    return usersQuerySnapshot.docs.map(user => user.data());
+const getGreeting = () => {
+    if (currentHour >= 5 && currentHour < 12) return 'Good Morning';
+    else if (currentHour >= 12 && currentHour < 17) return 'Good Afternoon';
+    else if (currentHour >= 17 && currentHour < 21) return 'Good Evening';
+    else return 'Good Night';
 };
-
-const fetchPosts = async () => {
-    const postsQuerySnapshot = await getDocs(collection(db, "posts"));
-    return postsQuerySnapshot.docs.map(doc => ({ ...doc.data(), docId: doc.id }));
-};
-
-
-// Render all blogs started
 
 const renderAllBlogs = async () => {
-    console.log("Rendering blogs...");
-    const allBlogsArray = await fetchPosts();
-    console.log("Fetched posts:", allBlogsArray);
+    const usersData = (await getDocs(collection(db, "users"))).docs.map(user => user.data());
+    const postsData = (await getDocs(collection(db, "posts"))).docs.map(post => ({ ...post.data(), docId: post.id }));
 
-    const userData = await fetchUserData();
-    console.log("Fetched users:", userData);
-
-    allBlogsArray.forEach(item => {
-        const user = userData.find(user => user.uid === item.uid);
-
-        console.log("User:", user);
-
-        const time = item.postDate.seconds;
-        const mydate = new Date(time * 1000);
-        const formattedDate = mydate.toLocaleDateString('en-US', {
+    postsData.forEach(async item => {
+        const user = usersData.find(user => user.uid === item.uid);
+        const postTime = new Date(item.postDate.seconds * 1000).toLocaleDateString('en-US', {
             month: 'short',
             day: 'numeric',
             year: 'numeric'
         });
+        const postImg = user ? user.profileUrl : '';
 
-        const postimg = user ? user.profileUrl : '';
-
-        AllBlogsContainer.innerHTML += `
-        <div class="bg-white p-8 rounded-lg mb-5 shadow-2xl max-w-xl ml-40 w-full">
-            <div class="flex gap-5">
-                <div class="mb-4 text-center">
-                    <img src="${postimg}" class="rounded-xl w-20 h-20 mb-4" id="blog-img">
+        const blogHTML = `
+            <div class="bg-white p-8 rounded-lg mb-5 shadow-2xl max-w-xl ml-40 w-full">
+                <div class="flex gap-5">
+                    <div class="mb-4 text-center">
+                        <img src="${postImg}" class="rounded-xl w-20 h-20 mb-4" id="blog-img">
+                    </div>
+                    <div class="w-1/2">
+                        <h2 class="text-xl font-bold text-[#212529]">${item.title}</h2>
+                        <h5 class="text-sm mt-1 text-[#6C757D]">${user.firstName} ${user.lastName} ${postTime}</h5>
+                    </div>
                 </div>
-                <div class="w-1/2">
-                    <h2 class="text-xl font-bold text-[#212529]">${item.title}</h2>
-                    <h5 class="text-sm mt-1 text-[#6C757D]">${user.firstName} ${user.lastName} ${formattedDate}</h5>
+                <p class="text-[#6C757D] text-sm mt-3 whitespace-normal break-words">${item.description}</p>
+                <div class="flex mt-3 text-sm">
+                    <a href="userblog.html" class="bg-transparent border-none text-[#7749F8]  mr-20" id="user-link">See all from this user</a>
                 </div>
-            </div>
-            <p class="text-[#6C757D] text-sm mt-3 whitespace-normal break-words">
-                ${item.description}
-            </p>
-            <div class="flex mt-3 text-sm">
-                <a href="userblog.html" class="bg-transparent border-none text-[#7749F8]  mr-20" id="user-link">See all from this user</a>
-            </div>
-        </div> `;
-
-        console.log("Rendering post:", item);
+            </div> `;
+        document.querySelector('#AllBlogsContainer').innerHTML += blogHTML;
     });
-
-    console.log("Finished rendering blogs.");
 };
 
-// Render all blogs ended
-
-
 document.addEventListener("DOMContentLoaded", async () => {
-    console.log("DOM loaded, rendering blogs...");
+    const greeting = getGreeting();
+    const greetingText = document.createTextNode(`${greeting} Readers!`);
+    time.appendChild(greetingText);
+
     await renderAllBlogs();
 });
+
 
